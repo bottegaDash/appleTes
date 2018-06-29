a4326c35ec165965b429484e79020bfc6bce9037
David Ash <bottegadash@gmail.com>
2018-06-28 20:36:25 -0400

    Added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,22 @@
 Rails.application.routes.draw do
-  resources :audit_logs
-  resources :posts
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :audit_logs, except: [:new, :edit, :destroy] do
+    member do
+      get :confirm
+    end
+  end
+
+  namespace :admin do
+      resources :users
+      resources :posts
+      resources :admin_users
+
+      root to: "users#index"
+    end
+  resources :posts do
+    member do
+      get :approve
+    end
+  end
+  devise_for :users, skip: [:registrations]
+  root to: "static#home"
 end
