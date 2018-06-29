dbb391fe623825b05617c6c744a8b6bb1dea7c2a
David Ash <bottegadash@gmail.com>
2018-06-28 20:44:42 -0400

    Added models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,10 @@
 class User < ApplicationRecord
+  has_many :posts
+  has_many :audit_logs
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
   validates_presence_of :first_name, :last_name, :phone
 
   PHONE_REGEX = /\A[0-9]*\z/
@@ -13,6 +14,6 @@ class User < ApplicationRecord
   validates :phone, length: { is: 10 }
 
   def full_name
-   last_name.upcase + ", " + first_name.upcase
+    last_name.upcase + ", " + first_name.upcase
   end
 end
